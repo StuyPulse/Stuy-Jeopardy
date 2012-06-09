0e6b5a540699e568ac90aaa9d2df706144e70eb8
Samuel Lijin <sxlijin@gmail.com>
2012-06-09 16:23:22 -0400

    updated but still missing required revisions

--- Jeopardy/Jeopardy.ino
+++ Jeopardy/Jeopardy.ino
@@ -1,6 +1,26 @@
-unsigned int contest_pin[] = {4,5,6,7}; // GPIO pins on buzzer #s 0-3
-unsigned int light_pin[] = {0,1,2,3}; // GPIO pins on light #s 0-3
-unsigned int moderator_pin = 11; // GPIO pin on the moderator button
+/**
+* Stuyvesant Jeopardy System
+* @author FRC694
+*
+* This system is a replica of Jeopardy and works as follows:
+* - There are 4 "contestants": 1 parent, 1 teacher, 1 alumnus, 1 team of students
+*   - The team of students consists of 4 students
+* - The moderator asks a question.
+* - The contestants buzz to answer the question
+*   - The buzzer sounds to indicate that the answering period has begun
+*   - The contestant has 5 seconds to answer the question
+*     - If the constestant answers correctly, the moderator presses his button, after which any contestant may buzz to answer again.
+*     - If the contestant answers falsely (or doesn't answer), everyone must wait until his questioning period is up, at which point the buzzer sounds twice and anyone else may answer.
+* - If any student answers falsely, the entire team of students is seen as having answered falsely.
+* - A contestant has 5 seconds to answer a question after buzzing and being selected to answer.
+*/
+
+unsigned int contest_pin[] = {5,6,7,8,9,10,11,12}; // GPIO pins on buzzer #s 0-3
+unsigned int moderator_pin = 5; // GPIO pin on the moderator button
+
+unsigned int contest_light[] = {0,1,2,3,4}; // GPIO pins on light #s 0-3
+unsigned int moderator_light = 0;
+
 unsigned int claxon_pin = 12; // GPIO pin on the speaker
 int i; // iterator
 
@@ -9,9 +29,10 @@ void setup() {
   
   for (i = 0; i < 4; i++)  {
     pinMode(contest_pin[i], INPUT); // allocate and configure contestant input lines
-    pinMode(light_pin[i], OUTPUT); // allocate and configure contestant output lines
+    pinMode(contest_light[i], OUTPUT); // allocate and configure contestant output lines
   }
-  pinMode(moderator_pin, OUTPUT); // allocate and configure moderator input
+  pinMode(moderator_pin, INPUT); // allocate and configure moderator input
+  pinMode(moderator_light, OUTPUT);
   pinMode(claxon_pin, OUTPUT); // allocate and configure speaker line
   
   i = 0; // initialize iterator
@@ -21,15 +42,21 @@ void loop() {
   if (isPressed(contest_pin[i])) {
     int start = millis();
     
-    digitalWrite(light_pin[i], HIGH); // turn the light on
+    digitalWrite(contest_light[i], HIGH); // turn the light on
     tone(claxon_pin, 150, 200); //------ pulse the speaker on
     
-    while (millis() - start <= 5000 && digitalRead(moderator_pin));
+    while (millis() - start < 5000) {
+      if (isPressed(moderator_pin))
+        break;
+    }
     // give contestant 5 seconds to respond
     
     soundEnd(claxon_pin);
+    digitalWrite(contest_light[i], LOW);
     // signal that the waiting period is over or question has been answered
   }
+  
+  i = (i +1) % 4; //
 }
 
 boolean isPressed(unsigned int pin) { // All the switches are pulled up
